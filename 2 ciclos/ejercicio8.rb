21b85d2330333428a15257ba3e7e8670615cfcbe
Richard Stuardo <stuardorichard@gmail.com>
2017-08-23 10:26:03 -0300

    actividad11 al 90%

--- 2 ciclos/ejercicio8.rb
+++ 2 ciclos/ejercicio8.rb
@@ -1,14 +1,7 @@
 # Generar -utilizando un ciclo iterativo- un string con la siguiente estructura:
 # a = '1impar 2par 3impar 4par 5impar 6par 7impar 8par 9impar 10par'
 
-a = ''
-10.times do |i|
-	if i.even?
- 	i += 1
- puts i + 'impar'
-   else 
-   puts 'par'
-  end
-end
 
-puts a
+a = ''
+(1..10).each { |i| a += i.odd? ? "#{i}impar " : "#{i}par " }
+print a
--- 3 ciclos anidados/ejercicio3.rb
+++ 3 ciclos anidados/ejercicio3.rb
@@ -2,3 +2,17 @@
 # la tabla de multiplicar del número ingresado. Debe repetir la operación hasta
 # que se ingrese un 0 (cero).
 # Ingrese un número (0 para salir): _
+
+puts 'ingrese número'
+puts 'ingresa 0 para salir'
+a = gets.chomp.to_i
+
+while a != 0 
+	10.times do |i|  
+		resul = a * (i+1) 
+		puts "#{a} x #{i +1} = #{resul}" 	 
+	end 
+	puts 'ingrese número'
+	puts 'ingresa 0 para salir'
+	a = gets.chomp.to_i
+end
